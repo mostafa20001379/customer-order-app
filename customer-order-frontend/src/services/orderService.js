19d9b265a8fa80e559d639941cb2d229bd82e5c6
mostafa20001379 <mostafa.ahoee1379@gmail.com>
2024-04-17 15:16:16 +0330

    Edit API_URL

--- customer-order-frontend/src/services/orderService.js
+++ customer-order-frontend/src/services/orderService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http:localhost:8000/api/orders';
+const API_URL = 'http://localhost:8000/api/orders';
 
 const getOrders = async () => {
     try {
         const response = await axios.get(API_URL);
-        return response.data;
+        return response.data.data;
     } catch (error) {
         throw new Error('Error fetching orders: ' + error.message);
     }
